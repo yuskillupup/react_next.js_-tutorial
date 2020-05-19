3662fecefbc33850a83bcffe4509ac40fade27e8
yuskillupup <yuworkeryu55@gmail.com>
2020-05-19 21:43:21 +0900

    test

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home({allPostsData}) {
           <title>{siteTitle}</title>
         </Head>
         <section className={utilStyles.headingMd}>
-          <p>Hello, I'm Yuta. I'm a software enginer.</p>
+          <p>Hello, I'm Yuka. I'm a software enginer.</p>
           <p>(This is a sample website - you’ll be building a site like this on{' '}
             <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
           </p>
